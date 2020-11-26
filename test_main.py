0e510d11d95ffdf18e2b8a68bcd8805c420bf3cb
AniaJezierska <candyrockstore5@gmail.com>
2020-11-26 21:40:53 +0100

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -9,8 +9,8 @@ import main
 
 SECRET = 'TestSecret'
 TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NjEzMDY3OTAsIm5iZiI6MTU2MDA5NzE5MCwiZW1haWwiOiJ3b2xmQHRoZWRvb3IuY29tIn0.IpM4VMnqIgOoQeJxUbLT-cRcAjK41jronkVrqRLFmmk'
-EMAIL = 'wolf@thedoor.com'
-PASSWORD = 'huff-puff'
+EMAIL = 'ania@gmail.com'
+PASSWORD = 'udacity'
 
 @pytest.fixture
 def client():
